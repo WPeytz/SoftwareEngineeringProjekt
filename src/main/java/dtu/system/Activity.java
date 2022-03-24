f2b964d88688f1481eeb6467b1f110945fdbd423
WPeytz <s204145@student.dtu.dk>
2022-03-24 22:17:37 +0100

    Changes by Nikolai

--- src/main/java/dtu/system/Activity.java
+++ src/main/java/dtu/system/Activity.java
@@ -1,5 +1,7 @@
 package dtu.system;
 
+
+
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -8,7 +10,37 @@ public class Activity /* extends Project */ {
     Calendar startDate, endDate;
     double timeBudget;
     int weekNumber;
-    ArrayList<DevelopmentEmployee> workingDevelopers;
+    ArrayList<DevEmp> workingDevelopers;
+    boolean externalActivity;
+    ArrayList<Double> timeSpent;
+
+
+    public Activity(String name, double timeBudget, boolean externalActivity) {
+        /* super(???); */
+        this.name  = name;
+        this.timeBudget = timeBudget;
+        startDate = Calendar.getInstance();
+        endDate = Calendar.getInstance();
+        weekNumber = startDate.get(Calendar.WEEK_OF_YEAR);
+        this.externalActivity = externalActivity;
+        timeSpent = new ArrayList<>();
+        workingDevelopers = new ArrayList<>();
+    }
+
+    public double activityTime() {
+        double sum = 0;
+        for (double d : timeSpent) {
+            sum += d;
+        }
+        return sum;
+    }
+
+    public void requestAssistance (DevEmp assistingDev) {
+        if (assistingDev.free) {
+            workingDevelopers.add(assistingDev);
+        }
+    }
+
 
 
 }
