75af58ccd1ef3bd76b9ed457fc89d335d310dd21
WPeytz <s204145@student.dtu.dk>
2022-03-24 20:46:31 +0100

    With Classes

--- src/main/java/dtu/system/Activity.java
+++ src/main/java/dtu/system/Activity.java
@@ -1,8 +1,14 @@
 package dtu.system;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Activity {
     String name;
-
+    Calendar startDate, endDate;
+    double timeBudget;
+    int weekNumber;
+    ArrayList<DevelopmentEmployee> workingDevelopers;
 
 
 }
